76998c72122d1a7c01cf7a50867f94011cd48e99
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2022-10-13 20:56:41 -0400

    fix copyright

--- frontend/src/components/layout/Footer.vue
+++ frontend/src/components/layout/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center p-6 bg-gray-200">
-        <span>© 2021 Copyright:</span>
+        <span>© 2022 Copyright:</span>
         <p class="text-gray-600 font-semibold">Zelzazor</p>
     </div>
 </template>
